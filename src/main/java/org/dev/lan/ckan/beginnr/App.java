5efd6a782bf12a250ea809ea0db1605279667f27
chatofrey <chatofrey@gmail.com>
2016-07-10 10:04:21 +0200

    change package'name; adding cobertura maven plugin

---
+++ src/main/java/org/dev/lan/ckan/beginnr/App.java
@@ -0,0 +1,13 @@
+package org.dev.lan.ckan.beginnr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
